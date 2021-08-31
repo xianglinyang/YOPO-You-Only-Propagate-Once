535a03cf620d62d4d50518f9688cd3cb7912a585
xianglin <yxlyzxl@gmail.com>
2021-08-31 01:37:26 -0400

    for DVI experiments

--- experiments/CIFAR10/res18.pgd10/train.py
+++ experiments/CIFAR10/res18.pgd10/train.py
@@ -17,12 +17,14 @@ from collections import OrderedDict
 
 
 if __name__ == "__main__":
-
+    # for reproduce purpose
+    torch.manual_seed(1331)
     DEVICE = torch.device('cuda:{}'.format(args.d))
     torch.backends.cudnn.benchmark = True
 
     net = create_network()
-    state_dict = torch.load("E:\\DVI_exp_data\\resnet18_cifar10\\Model\\Epoch_100\\subject_model.pth")
+    # 190-th epoch of cifar10 resnet18 training
+    state_dict = torch.load("/home/xianglin/DVI_exp_data/case_studies/adv_training_batch/subject_model.pth")
     net.load_state_dict(state_dict)
     net.to(DEVICE)
 
@@ -33,6 +35,21 @@ if __name__ == "__main__":
 
     ds_train = create_train_dataset(args.batch_size)
     ds_val = create_test_dataset(args.batch_size)
+    # save dataset
+    # train_data = torch.Tensor([]).to(DEVICE)
+    # train_label = torch.Tensor([]).to(DEVICE)
+    # test_data = torch.Tensor([]).to(DEVICE)
+    # test_label = torch.Tensor([]).to(DEVICE)
+    # for i, (data, labels) in enumerate(ds_train, 0):
+    #     train_data = torch.cat((train_data, data.to(DEVICE).detach()), 0)
+    #     train_label = torch.cat((train_label, labels.to(DEVICE).detach()), 0)
+    # for i, (data, labels) in enumerate(ds_val, 0):
+    #     test_data = torch.cat((test_data, data.to(DEVICE).detach()), 0)
+    #     test_label = torch.cat((test_label, labels.to(DEVICE).detach()), 0)
+    # torch.save(train_data, os.path.join("." , "training_dataset_data.pth"))
+    # torch.save(test_data, os.path.join(".", "testing_dataset_data.pth"))
+    # torch.save(train_label, os.path.join(".", "training_dataset_label.pth"))
+    # torch.save(test_label, os.path.join(".", "testing_dataset_label.pth"))
 
     TrainAttack = config.create_attack_method(DEVICE)
     EvalAttack = config.create_evaluation_attack_method(DEVICE)
@@ -44,19 +61,23 @@ if __name__ == "__main__":
 
     # eval before training
     eval_one_epoch(net, ds_val, DEVICE, EvalAttack)
-    torch.save(net.state_dict(), os.path.join(save_dir, "{:d}.pth".format(0)))
+    epoch_dir = os.path.join(save_dir, "Epoch_{}".format(0))
+    if not os.path.exists(epoch_dir):
+        os.mkdir(epoch_dir)
+    # torch.save(net.state_dict(), os.path.join(epoch_dir, "subject_model.pth"))
 
-    for n_epoch in range(1, 51, 1):
+    for n_epoch in range(1, 100, 1):
 
-        descrip_str = 'Training epoch:{}/{} -- lr:{}'.format(n_epoch, config.num_epochs,
-                                                                           lr_scheduler.get_lr()[0])
-        train_one_epoch(net, ds_train, optimizer, criterion, DEVICE,
-                        descrip_str, TrainAttack, adv_coef = args.adv_coef)
-        # if config.val_interval > 0 and n_epoch % config.val_interval == 0:
-        #     eval_one_epoch(net, ds_val, DEVICE, EvalAttack)
+        descrip_str = 'Training epoch:{}/{} -- lr:{}'.format(n_epoch, config.num_epochs,lr_scheduler.get_last_lr())
+                                                                           # lr_scheduler.get_lr()[0])
+        adv_dataset, adv_labels = train_one_epoch(net, ds_train, optimizer, criterion, DEVICE, descrip_str, TrainAttack,
+                                                  adv_coef = args.adv_coef, save=False)
         eval_one_epoch(net, ds_val, DEVICE, EvalAttack)
 
         lr_scheduler.step()
-
-        if n_epoch % 2 == 0:
-            torch.save(net.state_dict(), os.path.join(save_dir, "{:d}.pth".format(n_epoch)))
+        epoch_dir = os.path.join(save_dir, "Epoch_{}".format(n_epoch))
+        if not os.path.exists(epoch_dir):
+            os.mkdir(epoch_dir)
+        torch.save(net.state_dict(), os.path.join(epoch_dir, "subject_model.pth"))
+        torch.save(adv_dataset, os.path.join(epoch_dir, "adv_dataset.pth"))
+        torch.save(adv_labels, os.path.join(epoch_dir, "adv_labels.pth"))
